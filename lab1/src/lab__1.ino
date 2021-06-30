116b5f5a32dad15abd4265aa4dddac10c2203fb6
harigiat4 <harigiat4@gmail.com>
2021-06-30 19:17:11 +0300

    test

---
+++ lab1/src/lab__1.ino
@@ -0,0 +1,22 @@
+
+
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+
+void setup() {
+  
+
+ pinMode(D7, OUTPUT);
+
+
+}
+
+
+void loop() {
+ digitalWrite(D7, LOW);
+ delay(1000);
+ digitalWrite(D7, HIGH);
+ delay(1000);
+
+}
