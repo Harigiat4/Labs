6bd7db5fe63784e5c9edd60d86e588070052f967
Harigiat4 <86673122+Harigiat4@users.noreply.github.com>
2021-07-02 18:32:07 +0300

    Update lab4.ino

--- lab4/src/lab4.ino
+++ lab4/src/lab4.ino
@@ -1,26 +1,25 @@
 SYSTEM_MODE(MANUAL);
 SYSTEM_THREAD(ENABLED);
 
-
 bool value;
 
-void setup() {
+void setup()
+{
 
-  pinMode(D6, OUTPUT);  
-  pinMode(D7, INPUT);  
+  pinMode(D6, OUTPUT);
+  pinMode(D7, INPUT);
 }
 
+void loop()
+{
 
-
-void loop() {
-  
-  value = digitalRead(D7); 
-  if (value == true) { //if button is pressed
+  value = digitalRead(D7);
+  if (value == true)
+  {                         //if button is pressed
     digitalWrite(D6, HIGH); //turn light on
-
-
-  } else { //if button isn't pressed
+  }
+  else
+  {                        //if button isn't pressed
     digitalWrite(D6, LOW); //turn light off
   }
-
 }
