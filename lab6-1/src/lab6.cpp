5662bae949fd77045a825c29b5676fa7fde297ac
harigiat4 <harigiat4@gmail.com>
2021-07-05 13:59:13 +0300

    Add Lab6-1

---
+++ lab6-1/src/lab6.cpp
@@ -0,0 +1,44 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "c:/Users/PCD/Desktop/IOTEngineering/Labs/lab6/src/lab6.ino"
+#include "oled-wing-adafruit.h"
+
+void setup();
+void loop();
+#line 3 "c:/Users/PCD/Desktop/IOTEngineering/Labs/lab6/src/lab6.ino"
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+OledWingAdafruit display;
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+  // sets up OLED display, sets tmp36 as input 
+  pinMode(A4, INPUT);
+  display.display();  
+  display.clearDisplay();
+  display.setup();
+  Serial.begin(9600); 
+}
+
+
+void loop() {
+  // reads the tmp36 in a loop
+  display.loop();  
+  uint64_t reading = analogRead(A4);
+  double voltage = (reading * 3.3) / 4095.0;
+  double temperature = (voltage - 0.5) * 100;
+  //print the temperature
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0,0);
+  display.print("\nFahrenheit = ");
+  display.print((temperature *9/5)+32);
+  display.print("\n\nCelsius    = ");
+  display.print(temperature);
+  display.display();
+}             
